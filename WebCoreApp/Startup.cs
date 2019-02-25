3578c5ae53727220bbba33551165cb4d27312827
Trọng Thoại <Trng Thoi@DESKTOP-GQHELME>
2019-02-25 16:42:50 +0700

    create admin area

--- WebCoreApp/Startup.cs
+++ WebCoreApp/Startup.cs
@@ -99,6 +99,9 @@ namespace WebCoreApp
                 routes.MapRoute(
                     name: "default",
                     template: "{controller=Home}/{action=Index}/{id?}");
+
+                routes.MapRoute(name: "areaRoute",
+                    template: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
             });
             
             
